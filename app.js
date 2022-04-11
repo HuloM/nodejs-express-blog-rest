2b3e57b3a4e95b4f4abd7a681b88a079bde68a90
matt <matthieu.l.dubray@gmail.com>
2022-04-11 12:58:33 -0700

    connecting our app to mongoDB (the mongoDB connection string is in the .env file which will not be included for security reasons)

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const path = require('path')
 
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const MONGODB_URI = process.env.MONGODB_HOST
 
 const app = express()
 
 app.use(express.json())
-app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(8080)
+mongoose.connect(MONGODB_URI)
+    .then(result => {
+        console.log('Server Active')
+        app.listen(8080)
+    })
+    .catch(err => console.log(err))
